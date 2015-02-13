b2ca7cb07ab2e29354a4c7220f34cc5eaaae93d1
Scott Rice <scottrice10@gmail.com>
2015-02-12 22:13:25 -0800

    Update metadata.rb

--- mongodb/metadata.rb
+++ mongodb/metadata.rb
@@ -1,7 +1,7 @@
-name        "mongodb-mysite"
-description 'Example cookbook for MongoDB Berkshelf based install'
-maintainer  "MyOrg"
-license     "Apache 2.0"
+name        "mongodb"
+description 'Cookbook for Sphereable'
+maintainer  "Strange Lightning"
+license     "MIT"
 version     "1.0.0"
 
 depends 'mongodb'
